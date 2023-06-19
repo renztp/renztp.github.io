611333d93b97da7f9cb3b2a8463cd54eb5bd3115
renztp <tullaorenz@gmail.com>
2023-06-19 15:17:54 +0800

    added additional stylesheets and refactor code

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/dist");
+  eleventyConfig.addPassthroughCopy("src/dist/css");
+  eleventyConfig.addPassthroughCopy("src/dist/js");
 };
